ab5428d063368c61c92e813c1453e061374671f3
Daniel Schülke <daniel@schuelkeonline.de>
2015-01-17 15:05:25 +0100

    Added FontAwesome as SASS Files

--- UI/HTML/Gruntfile.js
+++ UI/HTML/Gruntfile.js
@@ -24,8 +24,8 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
-  grunt.registerTask('default', ['watch']);
+  grunt.registerTask('default', ['compass']);
   grunt.registerTask('sass', ['compass']);
-
+   grunt.registerTask('watch', ['watch']);
 
 };
